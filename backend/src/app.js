8c744bf3dea861e63dfb7c780b0da7e619399d4a
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-02-02 19:28:31 +0530

    models and routes boiler plate created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const postModel = require('./models/post.model');
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/create-post', (req, res) => {
+
+});
+
+app.get('/all-posts', (req, res) => {
+
+});
 
 
 module.exports = app;
